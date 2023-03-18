73865b3421c13391124eb7cc756ca3022aa3090f
Rijuth Menon <rijuthm@gmail.com>
2023-03-18 10:41:39 +0530

    creating an customPainter

---
+++ lib/object_painter.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ObjectPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {}
+
+  @override
+  bool shouldRepaint(ObjectPainter oldDelegate) {
+    return false;
+  }
+}
